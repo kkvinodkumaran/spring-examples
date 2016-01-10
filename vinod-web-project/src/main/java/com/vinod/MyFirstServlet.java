f792103be3dc20998da3488448898f3836d3475a
Vinod Kariyathungal Kumaran (V) <vinod_kariyathungalku@apple.com>
2016-01-10 12:41:25 -0800

    servlet

---
+++ vinod-web-project/src/main/java/com/vinod/MyFirstServlet.java
@@ -0,0 +1,47 @@
+package com.vinod;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFirstServlet
+ */
+public class MyFirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public MyFirstServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<HTML><TITLE>Http Servlet Example</TITLE><BODY>");
+		out.println("<H2>Hello world</H2><HR>");
+		out.println("</BODY><HTML>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
