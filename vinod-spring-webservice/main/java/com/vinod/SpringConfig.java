42629937aa1702677df478adf44c66d15a0b8c5d
Vinod Kariyathungal Kumaran (V) <vinod_kariyathungalku@apple.com>
2016-01-10 19:59:49 -0800

    updated config

--- vinod-spring-webservice/main/java/com/vinod/SpringConfig.java
+++ vinod-spring-webservice/main/java/com/vinod/SpringConfig.java
@@ -4,10 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = {
-        "com.vinod"})
-public class SpringConfig extends WebMvcConfigurerAdapter{
+@ComponentScan(basePackages = { "com.vinod" })
+public class SpringConfig {
 
 }
